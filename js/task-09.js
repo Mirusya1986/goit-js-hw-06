a57558700e33b1f7a40bcaf09f386107b97eda4f
Mirusya1986 <k.mirochka@gmail.com>
2021-12-09 22:56:15 +0200

    final

--- js/task-09.js
+++ js/task-09.js
@@ -1,18 +1,18 @@
-const body=document.body;
-const button = document.querySelector('.change-color');
-const color = document.querySelector('.color');
 
- 
-
-function getRandomHexColor(event) {
-
-randomHexColor = `${event.Target.value}`;
+function getRandomHexColor() {
 
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const spanColor = document.querySelector('.color');
+const buttonChangeColor = document
+.querySelector('.change-color')
+.addEventListener('click', onbuttonChangeColorClick);
+
+function onbuttonChangeColorClick(){
+  const color = getRandomHexColor();
+  document.body.style.backgroundColor = color;
+  spanColor.textContent = color;
+  console.log(spanColor.textContent);
+}
 
 
-// const onButton = event => {
-//   console.log(event);
-// } 
-// body.addEventListener('click', onButton);
